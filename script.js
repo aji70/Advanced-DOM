15d8113d6d1bd55688a752b7a6218c61bda19d47
Ajidokwu Sabo <113303791+aji70@users.noreply.github.com>
2023-12-05 01:10:52 +0100

    completed

--- script.js
+++ script.js
@@ -145,3 +145,102 @@ const stickyNav = function(entries){
 }
 const headerObserver = new IntersectionObserver(stickyNav, {root: null, threshold: 0, rootMargin:`-${navHeight}px` })
 headerObserver.observe(header);
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function(entries, observer){
+  const [entry] = entries;
+  if(!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {root:null, threshold:0.15})
+allsections.forEach(function(section){
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+})
+
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+const loadingImg = function(entries, observer){
+  const [entry] = entries;
+  if(!entry.isIntersecting) return;
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function(){
+    entry.target.classList.remove('lazy-img')
+  })
+  observer.unobserve(entry.target);
+};
+const imgObserver = new IntersectionObserver(loadingImg, {root:null, threshold:0, rootMargin:'200px'});
+imgTargets.forEach(img => imgObserver.observe(img));
+
+//  SLIDER
+const slider = function(){
+const slides = document.querySelectorAll('.slide'); /*getting all the slides*/
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+const dotContainer = document.querySelector('.dots')
+let curSlide = 0;
+const maxSlide = slides.length;
+
+const createDots = function(){
+  slides.forEach(function(_, i){
+    dotContainer.insertAdjacentHTML('beforeend',`<button class = "dots__dot" data-slide ="${i}"></button>`)
+  })
+}
+
+const activeDots = function(slide){
+  document.querySelectorAll('.dots__dot').forEach((dot => dot.classList.remove('dots__dot--active')))
+  document.querySelector(`.dots__dot[data-slide = "${slide}"]`).classList.add('dots__dot--active')
+}
+
+
+const goToSlide = function(slide){
+  slides.forEach((s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)) 
+}
+
+const nextSlide = function(){
+  if(curSlide === maxSlide - 1){
+    curSlide = 0;
+  }else{
+    curSlide++;
+  }
+  goToSlide(curSlide)
+  activeDots(curSlide)
+}
+
+const prevSlide = function(){
+  if(curSlide === 0){
+    curSlide = maxSlide - 1;
+  }else{
+    curSlide--;
+  }
+  goToSlide(curSlide);
+  activeDots(curSlide)
+}
+
+const init = function(){
+  goToSlide(0);
+  createDots();
+  activeDots(0);
+}
+init()
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
+
+document.addEventListener('keydown', function(e){
+  if(e.key === 'ArrowLeft') prevSlide()
+  e.key === 'ArrowRight' && nextSlide();
+})
+
+dotContainer.addEventListener('click', function(e){
+  if(e.target.classList.contains('dots__dot')){
+    const {slide} = e.target.dataset;
+    goToSlide(slide);
+    activeDots(slide);
+  }
+})
+}
+slider()
